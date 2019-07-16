933cf43b4360de4a201b44f6e16d63a5d8c48b5c
sivakumar-raju <52912495+sivakumar-raju@users.noreply.github.com>
2019-07-16 14:21:22 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,6 @@
-n=int(input())
-temp=n
-rev=0
-while(n>0):
-       digit=n%10
-       rev=rev*10+digit
-       n=n//10
-if(temp==rev):
-        print('yes')
- else:
-        print('no')
+n=list(input())
+k=list(reversed(n))
+if(n==k):
+	print('yes')
+else:
+	print('no')
